fff35038c7ec7150d1eaf4e41c7ec601b7f3fe4d
Donovan <donovan.courtney@gmail.com>
2025-05-08 09:35:20 -0500

    forgot to add the canvas

--- app/(root)/_components/OpenBookCanvas.tsx
+++ app/(root)/_components/OpenBookCanvas.tsx
@@ -2,46 +2,81 @@
 
 import {
   Backdrop,
+  BakeShadows,
   OrbitControls,
   PerspectiveCamera,
   SoftShadows,
 } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React, { Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { Suspense, useRef, useState } from "react";
 import OpenBookModel from "./OpenBookModel";
 import { MathUtils } from "three";
+import { ArtworkDocument } from "@/models/Artwork";
+import useScreenSize from "@/utils/useScreenSize";
+import { Clock } from "three";
 
-const OpenBookCanvas = () => {
+const AnimatedPointLight = () => {
+  const lightRef = useRef<any>();
+  const clockRef = useRef(new Clock());
+  const [intensity, setIntensity] = useState(0);
+
+  const targetIntensity = 0.3;
+  const delay = 1; // 1 second delay
+  const animationSpeed = 0.5;
+
+  useFrame(() => {
+    if (lightRef.current) {
+      const elapsedTime = clockRef.current.getElapsedTime();
+
+      if (elapsedTime >= delay) {
+        const progressAfterDelay = elapsedTime - delay;
+        const newIntensity = Math.min(
+          progressAfterDelay * animationSpeed,
+          targetIntensity
+        );
+
+        lightRef.current.intensity = newIntensity;
+      }
+    }
+  });
+
   return (
-    <Canvas className="bg-green-300 w-full aspect-video" shadows>
-      <PerspectiveCamera
-        position={[0, 1.7, 0.4]}
-        rotation={[MathUtils.degToRad(-75), 0, 0]}
-        makeDefault
-        fov={35}
-      />
-      <ambientLight intensity={0.3} />
-      <directionalLight
-        castShadow
-        position={[0, 1, 0]}
-        intensity={0.6}
-        shadow-mapSize={[2048, 2048]}
-      />
-      <SoftShadows size={10} samples={10} focus={0} />
-      {/* <OrbitControls /> */}
-      <OpenBookModel />
+    <pointLight
+      ref={lightRef}
+      intensity={intensity}
+      color={"#FCD916"}
+      position={[-0.3, 1.1, -0.5]}
+    />
+  );
+};
 
-      <Backdrop
-        position={[0, 0, -1]}
-        scale={2} // Scales the backdrop, 1 by default
-        scale-x={4} // Scales the backdrop on the x-axis, 1 by default
-        receiveShadow={true}
-        floor={0.5} // Stretches the floor segment, 0.25 by default
-        segments={20} // Mesh-resolution, 20 by default
-      >
-        <meshStandardMaterial color="#ffffff" />
-      </Backdrop>
-    </Canvas>
+const OpenBookCanvas = ({ artworks }: { artworks: ArtworkDocument[] }) => {
+  return (
+    <>
+      <div className="w-full h-full">
+        <Canvas className={`w-full h-full aspect-video }`} shadows>
+          <ambientLight intensity={0.5} />
+          <directionalLight
+            castShadow
+            position={[0, 2, -2]}
+            intensity={1}
+            shadow-mapSize={[1024, 1024]}
+          />
+          <AnimatedPointLight />
+          <SoftShadows size={20} samples={10} focus={1} />
+
+          <Suspense fallback={null}>
+            <PerspectiveCamera
+              position={[0, 1.85, 0.75]}
+              rotation={[MathUtils.degToRad(-65), 0, 0]}
+              makeDefault
+              fov={32}
+            />
+            <OpenBookModel artworks={artworks} />
+          </Suspense>
+        </Canvas>
+      </div>
+    </>
   );
 };
 
