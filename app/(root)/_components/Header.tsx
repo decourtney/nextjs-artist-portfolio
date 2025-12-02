0c49a65f74aedabf466dde0336bb558e0db3428a
Donovan <donovan.courtney@gmail.com>
2025-12-02 13:57:08 -0600

    Remove unused imports and animation logic from Header component for cleaner code

--- app/(root)/_components/Header.tsx
+++ app/(root)/_components/Header.tsx
@@ -1,50 +1,7 @@
-"use client";
-
-import { motion } from "framer-motion";
-import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import SectionSeparator from "./SectionSeparator";
-import { useEffect } from "react";
-import { usePreviousPathname } from "@/app/context/PreviousPathnameContext";
 
 const Header = () => {
-  const pathname = usePathname();
-  // const previousRoute =
-  //   typeof window !== "undefined"
-  //     ? sessionStorage.getItem("previousRoute")
-  //     : null;
-  const isSmallHeader = pathname.startsWith("/gallery");
-  const { previousPathname, setPreviousPathname } = usePreviousPathname();
-
-  // Check if the previous route is "/gallery/* to determine if motions initial value needs to be set to the same value as the animate value
-  // This is to prevent the header from animating when navigating gallery/pages and reversing the animation when navigating between /gallery and other pages"
-  // const isInitialSame = previousRoute?.startsWith("/gallery");
-  const isInitialSame = previousPathname?.startsWith("/gallery");
-
-  useEffect(() => {
-    if (pathname !== previousPathname) {
-      // sessionStorage.setItem("previousRoute", pathname);
-      setPreviousPathname(pathname);
-    }
-  }, [pathname]);
-
-  const headerVariants = {
-    large: {
-      height: "350px",
-      transition: {
-        duration: 0.5,
-        ease: "easeInOut",
-      },
-    },
-    small: {
-      height: "64px",
-      transition: {
-        duration: 0.5,
-        ease: "easeInOut",
-      },
-    },
-  };
-
   return (
     <header className="relative flex flex-col items-center justify-end mt-10 md:mt-20">
       <div className="max-w-4xl w-full px-4">
